e4764ba5af2899eb1a425f55a26006f140762fce
Kazuki Ikehara <bsb_k_1003@ezweb.ne.jp>
2024-07-27 15:48:52 +0900

    ドロップダウンメニューは出来たから、sorceryやる手前

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "tailwindcss/tailwind.css";
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('user-menu-button').addEventListener('click', function() {
+    var menu = document.getElementById('profileMenu');
+    menu.classList.toggle('hidden');
+  });
+});
