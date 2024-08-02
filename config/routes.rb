eaf4dbec8f8fd0888cbc58c447767c155b00306d
Kazuki Ikehara <bsb_k_1003@ezweb.ne.jp>
2024-08-03 01:21:33 +0900

    ログアウト機能も完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
-  get 'sessions/destroy'
+  get 'sessions/destroy', as: :logout
   get 'home/index'
   root 'home#index'
   
   get 'login' => 'sessions#new', as: :login
   post 'login' => "sessions#create"
-  delete 'logout' => 'sessions#destroy', as: :logout
   
-  # こちらを修正
   get 'users/sign_up', to: 'users#new', as: 'new_user_registration'
   post 'users', to: 'users#create'
 end
