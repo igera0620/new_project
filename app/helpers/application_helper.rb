a5d76d6d293c3bf8451271f00615bd708966243b
Kazuki Ikehara <bsb_k_1003@ezweb.ne.jp>
2025-02-09 22:10:19 +0900

    OGP画像　画像が反映されていなかったのを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Pluspo',
-      title: '都内スポーツ施設の横断検索サービス',
+      title: '筋肉は全てを解決する',
       reverse: true,
       charset: 'utf-8',
-      description: 'Pluspoを使えば「スポーツ・日時・場所」の好きな組み合わせで自由にスポーツ施設を検索できます。',
-      keywords: 'スポーツ,スポーツ施設,東京',
+      description: 'Pluspoは、AIが最適な筋トレメニューを提案し、トレーニングを楽しく継続できるサービスです。',
+      keywords: '筋トレ, フィットネス, トレーニング, ワークアウト',
       canonical: request.original_url,
       separator: '|',
       og: {
@@ -25,14 +25,14 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp.png'), # 配置するパスやファイル名によって変更すること
-        local: 'ja-JP'
+        image: image_url('ogp.png'), # OGP画像のパス（適宜変更）
+        locale: 'ja-JP'
       },
-      # Twitter用の設定を個別で設定する
+      # Twitter用の設定
       twitter: {
-        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
-        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('ogp.png') # 配置するパスやファイル名によって変更すること
+        card: 'summary_large_image', # 大きめの画像を表示するカード形式
+        site: '@your_twitter_account', # 公式Twitterアカウント（必要に応じて変更）
+        image: image_url('ogp.png') # OGP画像のパス（適宜変更）
       }
     }
   end
