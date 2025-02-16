05b5a49f5cf0e127da4d5d5529dd158ec7f1d4ae
igera0620 <bsb_k_1003@ezweb.ne.jp>
2025-02-16 07:41:00 +0000

    問い合わせ

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,10 @@
 class ContactMailer < ApplicationMailer
-  default to: ENV.fetch('TOMAIL', nil) # 送信先メールアドレス
-
   def send_mail(contact)
     @contact = contact
-    mail(from: @contact.email, subject: "【お問い合わせ】#{@contact.subject}")
+    mail(
+      to: ENV.fetch('TOMAIL') || 'default@example.com', # TOMAILがnilならデフォルトアドレスを指定
+      from: @contact.email,
+      subject: "【お問い合わせ】#{@contact.subject}"
+    )
   end
 end
