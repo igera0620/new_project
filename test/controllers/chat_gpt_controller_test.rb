b312f8fa4afacc2e214aa5fb8429b2c541d82c45
igera0620 <bsb_k_1003@ezweb.ne.jp>
2025-02-22 11:08:32 +0000

    minitestの削除

--- test/controllers/chat_gpt_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ChatGptControllerTest < ActionDispatch::IntegrationTest
-  test "should get ask" do
-    get chat_gpt_ask_url
-    assert_response :success
-  end
-end
