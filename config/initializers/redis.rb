8a931dee389408370dea1bbc2405067b569a5bf5
igera0620 <bsb_k_1003@ezweb.ne.jp>
2025-04-19 16:25:25 +0000

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 require 'redis'
 
-REDIS = Redis.new(url: ENV.fetch('REDIS_URL'))
+REDIS = Redis.new(url: ENV.fetch('REDIS_URL', 'rediss://default:AUUdAAIjcDEzMDdmODRlMDRlYWU0Y2NiYTI4ZDAwYjU4NDk1YzM4Y3AxMA@loving-dingo-17693.upstash.io:6379'))
